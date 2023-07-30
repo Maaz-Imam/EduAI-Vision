92b7c3d1162b2a0ce10df387e7660dd7bad9984c
HanYuanHsu <a0306g@gmail.com>
2023-07-30 03:36:51 -0700

    working on quizzes

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from Backend.CourseRecommendation import get_recommendation
+import os
+import json
+import sys
 
 app = Flask(__name__)
 
@@ -12,6 +15,36 @@ def index():
     else:
         return render_template('index.html', result=None, isPost=False)
 
+'''
+def process_score(req, quiz_questions):
+
+    score = 0
+    for i, 
+'''
+
+Quizzes_folder_path = os.path.join(os.path.dirname(__file__), 'Dataset', 'Quizzes')
+    
+@app.route('/quiz', methods=['GET', 'POST'])
+def show_quiz():
+    quiz_path = os.path.join(Quizzes_folder_path, 'python.json')
+    with open(quiz_path, 'r') as f:
+        quiz_questions = json.load(f)
+
+    if request.method == 'POST': # for form submission
+        selected_value = request.form.get(f'question_{1}', default=-1)
+        return render_template('quiz.html', selected_value=selected_value)
+
+
+    
+    return render_template('quiz.html', json_content=quiz_questions, selected_value=None)
+
+
+'''
+@app.route('/quiz/submit', methods=['POST'])
+def process_quiz():
+    selected_value = request.form.get(f'question_{1}', default=-1)
+    return render_template('quiz.html', selected_value=selected_value)
+'''
 
 # Run the app
 if __name__ == '__main__':
